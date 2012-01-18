5bf7cc3c2b6b39419dc72abe991041f7a18b2535
Paul Cook <paul.cook@alternateversion.com>
2012-01-18 14:48:48 -0500

    added precompile config for print and ie manifests

--- lib/osucom_layout_rails/engine.rb
+++ lib/osucom_layout_rails/engine.rb
@@ -2,6 +2,7 @@ require "osucom_layout_rails/app_controller"
 
 module OsucomLayoutRails
   class Engine < Rails::Engine
+    config.assets.precompile += ['application-ie.css','application-print.css']
     initializer 'myengine.app_controller' do |app|
       ActiveSupport.on_load(:action_controller) do
         helper_method "current_user", "logged_in?"
