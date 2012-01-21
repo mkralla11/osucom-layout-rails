6f0f6c571a6b6b223f3b75178183fc8e264238cd
Paul Cook <paul.cook@alternateversion.com>
2012-01-21 16:32:53 -0500

    making layout more configurable from other apps

--- lib/osucom_layout_rails/engine.rb
+++ lib/osucom_layout_rails/engine.rb
@@ -5,6 +5,11 @@ module OsucomLayoutRails
     
     initializer 'myengine.app_controller' do |app|
       app.config.assets.precompile += ['application-ie.css','application-print.css']
+      
+      app.config.osucom_layout.site_title = "OSUCOM Layout"
+      app.config.osucom_layout.header_title = "OSU COM"
+      app.config.osucom_layout.header_subtitle = "College of Medicine"
+      
       ActiveSupport.on_load(:action_controller) do
         helper_method "current_user", "logged_in?"
         include OsucomLayoutRails::AppController::InstanceMethods
--- lib/osucom_layout_rails/view_helper.rb
+++ lib/osucom_layout_rails/view_helper.rb
@@ -3,5 +3,13 @@ module OsucomLayoutRails
     def title(default)
       @title && "#{@title} | #{default}" || default
     end
+    
+    def header_title
+      OsucomLayoutRails::Application.config.header_title
+    end
+    
+    def header_subtitle
+      OsucomLayoutRails::Application.config.header_subtitle
+    end
   end
 end
